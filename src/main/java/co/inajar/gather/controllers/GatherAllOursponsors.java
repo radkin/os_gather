0f8596d17a38d62db68bf4df19c7b6e8b6f4b42e
Noel Miller <noeldamonmiller@gmail.com>
2024-03-08 13:33:53 +0000

    add_concurrent_pipeline Per the diagram, added a pipeline of forked, joined, merged and in some cases sync bits

--- src/main/java/co/inajar/gather/controllers/GatherAllOursponsors.java
+++ src/main/java/co/inajar/gather/controllers/GatherAllOursponsors.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.management.ManagementFactory;
 import java.util.Collection;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
@@ -43,32 +44,37 @@ public class GatherAllOursponsors {
     private String getOpenSecretsContributors() { return "Open Secrets Contributors"; }
     private String getFecCampaign() { return "FEC Campaign"; }
 
+    private int getNumberOfThreads() { return ManagementFactory.getThreadMXBean().getThreadCount(); }
+
     @GetMapping("/gather-all-oursponsors")
     public String virtual() {
 
         // gather senators
         log.info("gathering all sponsors, start on {}", Thread.currentThread());
+        log.info("Parent thread is {}", Thread.currentThread().getThreadGroup().getName());
 
-        log.info("=== STEP1: ProPublica REST APIs start here ===");
-        System.out.println("gathering senators");
+        log.info("=== STEP1: ProPublica REST APIs start here === threadcount: {}", getNumberOfThreads());
+        System.out.println("gathering representatives");
         var senators = getSenators();
+        var congress = getCongress();
 
+        // This is where we would use the SYNC senators and congress results as
+        // parameters into our CompletableFutures below
+        log.info("=== STEP2: Fork Node parallel Open Secrets and FEC REST APIs start here === threadcount: {}", getNumberOfThreads());
         // for node kicks off download of open secrets: campaign, sectors, contributors. FEC: campaign
         CompletableFuture<String> senatorOpenSecretsCampaignFuture = supplyAsync(this::getOpenSecretsCampaign, newVirtualThreadPerTaskExecutor());
         CompletableFuture<String> senatorOpenSecretsSectorsFuture = supplyAsync(this::getOpenSecretsSectors, newVirtualThreadPerTaskExecutor());
         CompletableFuture<String> senatorOpenSecretsContributorsFuture = supplyAsync(this::getOpenSecretsContributors, newVirtualThreadPerTaskExecutor());
         CompletableFuture<String> senatorFecCampaignFuture = supplyAsync(this::getFecCampaign, newVirtualThreadPerTaskExecutor());
 
-
-        System.out.println("gathering congress");
-        var congress = getCongress();
-
+        log.info("=== We should have threads here === threadcount: {}", getNumberOfThreads());
         // for node kicks off download of open secrets: campaign, sectors, contributors. FEC: campaign
         CompletableFuture<String> congressOpenSecretsCampaignFuture = supplyAsync(this::getOpenSecretsCampaign, newVirtualThreadPerTaskExecutor());
         CompletableFuture<String> congressOpenSecretsSectorsFuture = supplyAsync(this::getOpenSecretsSectors, newVirtualThreadPerTaskExecutor());
         CompletableFuture<String> congressOpenSecretsContributorsFuture = supplyAsync(this::getOpenSecretsContributors, newVirtualThreadPerTaskExecutor());
         CompletableFuture<String> congressFecCampaignFuture = supplyAsync(this::getFecCampaign, newVirtualThreadPerTaskExecutor());
 
+        log.info("=== STEP3: Join Node Open Secrets and FEC results start here === threadcount: {}", getNumberOfThreads());
         var sosCampaignF = senatorOpenSecretsCampaignFuture.join();
         var sosSectorsF = senatorOpenSecretsSectorsFuture.join();
         var sosContributorsF = senatorOpenSecretsContributorsFuture.join();
@@ -79,6 +85,7 @@ public class GatherAllOursponsors {
         var cosContributorsF = congressOpenSecretsContributorsFuture.join();
         var cfCampaignF = congressFecCampaignFuture.join();
 
+        log.info("=== STEP4: Merge Node data from our Join Node starts here === threadcount: {}", getNumberOfThreads());
         String result =
             sosCampaignF
                 .concat(sosCampaignF)
@@ -91,9 +98,6 @@ public class GatherAllOursponsors {
                 .concat(cosContributorsF)
                 .concat(cfCampaignF);
 
-//        String result = concatenate(senatorOpenSecretsCampaignFuture.join(), senatorOpenSecretsSectorsFuture.join(), senatorOpenSecretsContributorsFuture.join(),
-//                senatorFecCampaignFuture.join(), congressOpenSecretsCampaignFuture.join(), congressOpenSecretsSectorsFuture.join(),
-//                congressOpenSecretsContributorsFuture.join(), congressFecCampaignFuture.join());
         log.info("returning: {}", result, Thread.currentThread());
         return result;
     }
