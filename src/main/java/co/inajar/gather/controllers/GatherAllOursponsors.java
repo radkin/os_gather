b7e4cd2d346246ee4a8d8fc55a9022b26ff33e07
Noel Miller <noeldamonmiller@gmail.com>
2024-03-08 12:36:24 +0000

    add_concurrent_pipeline adderd the fork and join nodes.

--- src/main/java/co/inajar/gather/controllers/GatherAllOursponsors.java
+++ src/main/java/co/inajar/gather/controllers/GatherAllOursponsors.java
@@ -5,7 +5,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static co.inajar.gather.controllers.GatherAllOursponsors.Rep.concatenate;
 import static java.util.concurrent.CompletableFuture.supplyAsync;
@@ -27,22 +31,69 @@ public class GatherAllOursponsors {
     private String getSenators() {
         return "Senators";
     }
-
     private String getCongress() {
         return "Congress";
     }
+
+    // Note: these are a serious oversimplification because there probably
+    //      needs to be a specific method for each rep type
+
+    private String getOpenSecretsCampaign() { return "Open Secrets Campaign"; }
+    private String getOpenSecretsSectors() { return "Open Secrets Sectors"; }
+    private String getOpenSecretsContributors() { return "Open Secrets Contributors"; }
+    private String getFecCampaign() { return "FEC Campaign"; }
 
     @GetMapping("/gather-all-oursponsors")
     public String virtual() {
 
         // gather senators
         log.info("gathering all sponsors, start on {}", Thread.currentThread());
+
+        log.info("=== STEP1: ProPublica REST APIs start here ===");
         System.out.println("gathering senators");
+        var senators = getSenators();
 
-        CompletableFuture<String> senatorFuture = supplyAsync(this::getSenators, newVirtualThreadPerTaskExecutor());
-        CompletableFuture<String> congressFuture = supplyAsync(this::getCongress, newVirtualThreadPerTaskExecutor());
+        // for node kicks off download of open secrets: campaign, sectors, contributors. FEC: campaign
+        CompletableFuture<String> senatorOpenSecretsCampaignFuture = supplyAsync(this::getOpenSecretsCampaign, newVirtualThreadPerTaskExecutor());
+        CompletableFuture<String> senatorOpenSecretsSectorsFuture = supplyAsync(this::getOpenSecretsSectors, newVirtualThreadPerTaskExecutor());
+        CompletableFuture<String> senatorOpenSecretsContributorsFuture = supplyAsync(this::getOpenSecretsContributors, newVirtualThreadPerTaskExecutor());
+        CompletableFuture<String> senatorFecCampaignFuture = supplyAsync(this::getFecCampaign, newVirtualThreadPerTaskExecutor());
 
-        String result = concatenate(senatorFuture.join(), congressFuture.join());
+
+        System.out.println("gathering congress");
+        var congress = getCongress();
+
+        // for node kicks off download of open secrets: campaign, sectors, contributors. FEC: campaign
+        CompletableFuture<String> congressOpenSecretsCampaignFuture = supplyAsync(this::getOpenSecretsCampaign, newVirtualThreadPerTaskExecutor());
+        CompletableFuture<String> congressOpenSecretsSectorsFuture = supplyAsync(this::getOpenSecretsSectors, newVirtualThreadPerTaskExecutor());
+        CompletableFuture<String> congressOpenSecretsContributorsFuture = supplyAsync(this::getOpenSecretsContributors, newVirtualThreadPerTaskExecutor());
+        CompletableFuture<String> congressFecCampaignFuture = supplyAsync(this::getFecCampaign, newVirtualThreadPerTaskExecutor());
+
+        var sosCampaignF = senatorOpenSecretsCampaignFuture.join();
+        var sosSectorsF = senatorOpenSecretsSectorsFuture.join();
+        var sosContributorsF = senatorOpenSecretsContributorsFuture.join();
+        var sfCampaignF = senatorFecCampaignFuture.join();
+
+        var cosCampaignF = congressOpenSecretsCampaignFuture.join();
+        var cosSectorsF = congressOpenSecretsSectorsFuture.join();
+        var cosContributorsF = congressOpenSecretsContributorsFuture.join();
+        var cfCampaignF = congressFecCampaignFuture.join();
+
+        String result =
+            sosCampaignF
+                .concat(sosCampaignF)
+                .concat(sosSectorsF)
+                .concat(sosContributorsF)
+                .concat(sfCampaignF)
+
+                .concat(cosCampaignF)
+                .concat(cosSectorsF)
+                .concat(cosContributorsF)
+                .concat(cfCampaignF);
+
+//        String result = concatenate(senatorOpenSecretsCampaignFuture.join(), senatorOpenSecretsSectorsFuture.join(), senatorOpenSecretsContributorsFuture.join(),
+//                senatorFecCampaignFuture.join(), congressOpenSecretsCampaignFuture.join(), congressOpenSecretsSectorsFuture.join(),
+//                congressOpenSecretsContributorsFuture.join(), congressFecCampaignFuture.join());
         log.info("returning: {}", result, Thread.currentThread());
         return result;
     }
